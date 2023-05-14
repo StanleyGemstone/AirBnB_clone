59d987f4c92fd7f80fe01055a19f689845feee75
StanleyGemstone <stanleynduchi@gmail.com>
2023-05-14 13:26:26 +0100

    edited console

--- console.py
+++ console.py
@@ -5,13 +5,20 @@
 
 import cmd
 import sys
+from models.base_model import BaseModel
 
-
-class Console(cmd.Cmd):
-    prompt = ""
-
+class HBNBCommand(cmd.Cmd):
     if sys.stdin.isatty():
         prompt = "(hbnb) "
+    class_dic = [
+                    "BaseModel",
+                    "User",
+                    "Place",
+                    "State",
+                    "City",
+                    "Amenity",
+                    "Review"
+    ]
 
     def preloop(self):
         """command to be excuted before cmdloop invocation"""
@@ -33,10 +40,61 @@ class Console(cmd.Cmd):
         "Exit"
         return True
 
+    def do_create(self, line):
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
+        try:
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Usage: show <class> <id> or <class>.show(<id>)
+        Display the string representation of a class instance of a given id.
+        """
+        argl = parse(arg)
+        objdict = storage.all()
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict:
+            print("** no instance found **")
+        else:
+            print(objdict["{}.{}".format(argl[0], argl[1])])
+
     def do_quit(self, line):
         """The quit command terminate the Console"""
         sys.exit(0)
 
 
 if __name__ == '__main__':
-    Console().cmdloop()
+    HBNBCommand().cmdloop()
